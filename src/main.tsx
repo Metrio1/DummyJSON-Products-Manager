c4a9b2a0e4df51ba22b23ba685d3ccc0621591b4
Dmitriy_Zakirov <metrio@inbox.ru>
2026-07-08 17:41:42 +0500

    Switch to BrowserRouter with basename and update workflow for 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '@styles/main.scss';
@@ -10,9 +10,9 @@ import { queryClient } from './api/queryClient';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter basename="/DummyJSON-Products-Manager">
         <App />
-      </HashRouter>
+      </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
